f00fdf6807b14ae88fa8e2601dc5ca1664bb0a4f
cthulhucalling <cthulhucalling@gmail.com>
2016-03-21 15:05:52 -0600

    Added files via upload

--- flexo.py
+++ flexo.py
@@ -68,68 +68,86 @@ from collections import Counter
 max_threads = 100
 MINTTL=60
 MINACOUNT=8
-
+dnsid=[]
 packetqueue = Queue()
 
 def queue_packet(packet):
     packetqueue.put(packet)
 
-def analyze_packet(q,i):
-    while True:
-        packet = q.get()
-        #print packet.sprintf("Thread "+str(i)+" %IP.src% %IP.dst%\r\n")
- 	if packet.haslayer("DNSQR"):
-		domain = string.lower(packet[DNSQR].qname)
-		#print domain
-		lendom = len(domain)
-		if domain.endswith("."):
-			domain = domain[:-1]
-		if domain.count(".")>=1:
-			domain = ".".join( domain.split(".")[-2:])
-		#print domain
-		if domain in watch_domains:
-			print "[*] ALERT: Requested domain is in watchlist %s \n" % (domain)
-		if not domain in alexa_top_million and not domain.endswith("localdomain"):
-			#print "[*] ALERT: Domain Not in the top 1 million: %s \n" % (domain)
-			file_handle=open("not_in_alexa.save","a")
-			file_handle.write(domain+","+packet[DNSQR].qname+"\r\n")
-			file_handle.flush()
-			file_handle.close()
-		#if lendom > 60:
-			#print "[*] ALERT: Long Domain name > 60 characters: %s \n" % (domain)
-		if not domain_freq[domain]:
-			#print "[*] ALERT: New Domain detected %s.   Full hostname is %s" % ( domain, packet[DNSQR].qname )
-			domain_freq.update([ domain ])
-			file_handle = open( "domains.save", "w" )
-			pickle.dump(domain_freq, file_handle )
-			file_handle.flush()
-			file_handle.close()
-
-	if packet.haslayer("DNSRR"):
-		RED="\033[91m"
-		END="\033[0m"
-                ttl=packet.getlayer(DNSRR).ttl
-                rrname=packet.getlayer(DNSRR).rrname
-                count=packet.getlayer(DNS).ancount
-
-                if rrname.endswith("."):
-                        domain=rrname[:-1]
-
-                if domain.count(".") > 1:
-                        domain=".".join(domain.split(".")[-2:])
-
-                if count > 0:
-                        #Detect single flux networks
-                        if count > MINACOUNT and ttl < MINTTL and not domain in whitelist:
-                                print RED+"Request for "+str(domain)+" with TTL of "+str(ttl)+" and record count of "+str(count)+END
-                        #else:
-                        #       print "Request for "+str(domain)+" with TTL of "+str(ttl)+" and record count of "+str(count)
-                        #Try to detect double flux network
-	#print "Queue size: "+str(q.qsize())+"\r\n"
-        q.task_done()
+def analyze_packet(q):
+	while True:
+        	packet = q.get()
+		if packet.haslayer("DNS"):
+			#First check the whitelist
+			domain = string.lower(packet[DNSQR].qname)
+			if domain.endswith("."):
+				domain = domain[:-1]
+			if domain.count(".")>=1:
+				domain = ".".join( domain.split(".")[-2:])
+			if not domain in whitelist:
+				if (packet.getlayer("DNS").qr == 0):
+					#Packet is a query
+					dnsid.append(str(hex(packet.getlayer("DNS").id)))
+					lendom = len(domain)
+        	                	if domain in watch_domains:
+        	                	        print "[*] ALERT: Requested domain is in watchlist %s \n" % (domain)
+        	                	if not domain in alexa_top_million and not domain.endswith("localdomain"):
+        	                	        #print "[*] ALERT: Domain Not in the top 1 million: %s \n" % (domain)
+                	        	        file_handle=open("not_in_alexa.save","a")
+                	        	        file_handle.write(domain+","+packet[DNSQR].qname+"\r\n")
+                	        	        file_handle.flush()
+                	        	        file_handle.close()
+                	        	if lendom > 60:
+                	        	        print "[*] ALERT: Hostname > 60 characters: %s \n" % (packet[DNSQR].qname)
+                	        	if not domain_freq[domain]:
+                	        	        #print "[*] ALERT: New Domain detected %s.   Full hostname is %s" % ( domain, packet[DNSQR].qname )
+                	        	        domain_freq.update([ domain ])
+                	        	        file_handle = open( "domains.save", "w" )
+                	        	        pickle.dump(domain_freq, file_handle )
+                	        	        file_handle.flush()
+                	        	        file_handle.close()
+				elif (packet.getlayer("DNS").qr == 1):
+					#Packet is a response
+					if str(hex(packet.getlayer("DNS").id)) in dnsid:
+                	                	#print "ID %s found in valid ID table" % (hex(packet.getlayer("DNS").id))
+                	                	dnsid.remove(str(hex(packet.getlayer("DNS").id)))
+                	        	else:
+                	                        if (packet.getlayer("DNS").rcode == 0):
+                	                                print "ID %s unsolicited response for host %s" %(hex(packet.getlayer(DNS).id),packet.getlayer(DNSRR).rrname)
+                	                        elif (packet.getlayer("DNS").rcode == 1):
+                	                                print "ID %s returned unsolicited format-error for host %s" %(hex(packet.getlayer(DNS).id),packet.getlayer(DNSQR).qname)
+                	                        elif  (packet.getlayer("DNS").rcode == 2):
+                	                                print "ID %s returned unsolicited server-error for host %s" %(hex(packet.getlayer(DNS).id),packet.getlayer(DNSQR).qname)
+                	                        elif (packet.getlayer("DNS").rcode == 3):
+                	                                print "ID %s returned unsolicited name-error for host %s" %(hex(packet.getlayer(DNS).id),packet.getlayer(DNSQR).qname)
+                	                        elif (packet.getlayer("DNS").rcode == 4):
+                	                                print "ID %s returned unsolicited not-implemented-error for host %s" %(hex(packet.getlayer(DNS).id),packet.getlayer(DNSQR).qname)
+                	                        elif (packet.getlayer("DNS").rcode == 5):
+                	                                print "ID %s returned unsolicited refused-error for host %s" %(hex(packet.getlayer(DNS).id),packet.getlayer(DNSQR).qname)
+					if packet.haslayer("DNSRR"):
+                	        		RED="\033[91m"
+                	        		END="\033[0m"
+                	        		ttl=packet.getlayer(DNSRR).ttl
+                	        		rrname=packet.getlayer(DNSRR).rrname
+                	        		count=packet.getlayer(DNS).ancount
+	
+                	        		if rrname.endswith("."):
+               		         		        domain=rrname[:-1]
+	
+	        	                	if domain.count(".") > 1:
+	        	                	        domain=".".join(domain.split(".")[-2:])
+	
+	        	                	if count > 0:
+	        	                	        #Detect single flux networks
+	        	                	        if count > MINACOUNT and ttl < MINTTL and not domain in whitelist:
+	        	                	                print RED+"Request for "+str(domain)+" with TTL of "+str(ttl)+" and record count of "+str(count)+END
+				else:
+					#Packet is weird
+					print "Packet is weird"
+		q.task_done()
 
 for i in range(max_threads):
-    worker = Thread(target=analyze_packet, args=(packetqueue,i))
+    worker = Thread(target=analyze_packet, args=(packetqueue,))
     worker.setDaemon(True)
     worker.start()
 
@@ -161,4 +179,4 @@ except:
     domain_freq = Counter()
 
 print "I am running..."
-sniff(iface="eth4", filter="udp port 53", store=0, prn=queue_packet)
+sniff(iface="eth5", filter="udp port 53", store=0, prn=queue_packet)
